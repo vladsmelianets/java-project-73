212f474715911cce5470483e0b53ea0c9c182f04
vladsmelianets <vlad.smelianets@yandex.ru>
2023-02-28 18:30:15 +0300

    feat: add User model, add liquibase migration, adjust configs

---
+++ app/src/main/java/hexlet/code/model/User.java
@@ -0,0 +1,36 @@
+package hexlet.code.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.Instant;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String password;
+
+    @CreationTimestamp
+    @Column(nullable = false)
+    private Instant createdAt;
+}
