c1d3dd4670af9e1dc08ad778a2772759709db6ac
vladsmelianets <vlad.smelianets@yandex.ru>
2023-05-27 15:29:16 +0300

    test: replace @Transactional on class level @DirtiesContext, add test for update user
    @Transactional rolls back db changes, but doesn't reset identity counter. So tests involving id's could fail

--- app/src/test/java/hexlet/code/controller/UserControllerTest.java
+++ app/src/test/java/hexlet/code/controller/UserControllerTest.java
@@ -1,5 +1,7 @@
 package hexlet.code.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,26 +11,29 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.MediaType;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.jdbc.JdbcTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 
-import javax.transaction.Transactional;
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @ActiveProfiles("test")
-@Transactional
 @AutoConfigureMockMvc
 @Sql("classpath:data.sql")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class UserControllerTest {
 
     @Autowired
@@ -39,6 +44,23 @@ public class UserControllerTest {
 
     @Value(value = "${base-url}")
     private String baseUrl;
+
+    private static Map<String, String> validUser;
+    private static Map<String, String> nonValidUserMap;
+
+    private static final int ONE = 1;
+
+    @BeforeAll
+    static void setUpUserMaps() throws IOException {
+        final ObjectMapper mapper = new ObjectMapper();
+        final File validUserFile = new ClassPathResource("valid_user.json").getFile();
+        final String validUserJson = Files.readString(validUserFile.toPath());
+        final File nonValidUserFile = new ClassPathResource("non_valid_user.json").getFile();
+        final String nonValidUserJson = Files.readString(nonValidUserFile.toPath());
+
+        validUser = mapper.readValue(validUserJson, Map.class);
+        nonValidUserMap = mapper.readValue(nonValidUserJson, Map.class);
+    }
 
     @Test
     void shouldRespondAllUsersJsonWhenGetUsers() throws Exception {
@@ -93,11 +115,11 @@ public class UserControllerTest {
         assertThat(response.getContentType()).as("response body should be json")
                 .isEqualTo(MediaType.APPLICATION_JSON.toString());
         assertThat(response.getContentAsString()).as("response should contain new user")
-                .contains(TestData.VALID_FIRSTNAME, TestData.VALID_LASTNAME, TestData.VALID_EMAIL);
+                .contains(validUser.get("firstName"), validUser.get("lastName"), validUser.get("email"));
 
         int usersCountAfterRequest = JdbcTestUtils.countRowsInTable(jdbcTemplate, "users");
         assertThat(usersCountAfterRequest).as("valid user should be added to database")
-                .isEqualTo(usersCountBeforeRequest + TestData.ONE);
+                .isEqualTo(usersCountBeforeRequest + ONE);
     }
 
     @Test
@@ -120,12 +142,27 @@ public class UserControllerTest {
                 .isEqualTo(usersCountBeforeRequest);
     }
 
-    interface TestData {
+    @Test
+    void shouldUpdateUserWhenPutValidExistentUserJson() throws Exception {
+        final File jsonFile = new ClassPathResource("valid_user.json").getFile();
+        final String userToUpdate = Files.readString(jsonFile.toPath());
 
-        void suppressCheckstyle(); //TODO figure out how to suppress this f***ing checkstyle rule
-        int ONE = 1;
-        String VALID_FIRSTNAME = "valid firstname";
-        String VALID_LASTNAME = "valid lastname";
-        String VALID_EMAIL = "valid@email.com";
+        MockHttpServletResponse response = mockMvc
+                .perform(put(baseUrl + "/users/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(userToUpdate))
+                .andReturn()
+                .getResponse();
+
+        assertThat(response.getStatus()).as("response status should be 200/Ok").isEqualTo(200);
+        assertThat(response.getContentType()).as("response body should be json")
+                .isEqualTo(MediaType.APPLICATION_JSON.toString());
+        assertThat(response.getContentAsString()).as("response should contain updated user")
+                .contains(validUser.get("firstName"), validUser.get("lastName"), validUser.get("email"));
+
+        int updatedUserCount = JdbcTestUtils.countRowsInTableWhere(jdbcTemplate, "users",
+                "first_name = " + validUser.get("firstName"));
+        assertThat(updatedUserCount).as("user should be updated")
+                .isEqualTo(ONE);
     }
 }
